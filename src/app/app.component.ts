90037045b611f0e594eebfa4ebca7c3326220410
ZakariaZair <zakaria.zair@polymtl.ca>
2024-07-18 14:11:34 -0400

    feat: initial list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { exercise } from "./interface";
+import exos from "../assets/exos.json";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  title = 'repo';
+export class AppComponent implements OnInit {
+  title = "MFL";
+  mflTargetedMuscleSite: string;
+  exercises: exercise[];
+  constructor() {
+    this.exercises = [...exos.GymExercises, ...exos.FullBodyExercises];
+    this.mflTargetedMuscleSite =
+      "https://musclewiki.com/workout-generator/targeted-muscles";
+  }
+
+  ngOnInit() {}
+
+  copyToClipboard(set: string) {
+    navigator.clipboard.writeText(set);
+  }
+
+  isEmbedded(): boolean {
+    return window.self !== window.top;
+  }
+
+  isEmbeddedInNotion(): boolean {
+    return document.referrer.includes("notion.so");
+  }
 }
---
+++ src/app/interface.ts
@@ -0,0 +1,5 @@
+export interface exercise {
+  name: string;
+  sets: string[];
+  type: string;
+}
